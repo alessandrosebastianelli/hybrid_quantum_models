5b4f6360b72f97054fb1080c852c921fda420997
Alessandro Sebastianelli <33869842+alessandrosebastianelli@users.noreply.github.com>
2023-08-31 10:26:46 +0200

    replaced with ai_interface

--- hqm/layers/torch_layer.py
+++
@@ -1,6 +0,0 @@
-import pennylane as qml
-
-def torch_layer(circuit, n_qubits, n_layers):
-    weight_shape = {"weights": (n_layers, n_qubits)}
-    qlayer = qml.qnn.TorchLayer(circuit, weight_shape)
-    return qlayer
