84af8cef9eb1b67e00a195bfcbf62c3127d539d3
Alessandro Sebastianelli <33869842+alessandrosebastianelli@users.noreply.github.com>
2023-08-31 15:11:36 +0200

    fixing docstring

--- hqm/circuits/angle_encoding.py
+++ hqm/circuits/angle_encoding.py
@@ -6,17 +6,17 @@ import sys
 
 sys.path += ['.', './layers/']
 
-from layers.ai_interface import ai_interface
+from hqm.layers.ai_interface import ai_interface
 
 class BasicEntangledCircuit:
     '''
         This class implements a torch/keras quantum layer using a basic entangler
         circuit, as below:
 
-        0: ──RX(x1)──RX(w1.1)─╭●───────╭X──RX(w1.n)─╭●───────╭X─┤  <Z>
-        1: ──RX(x2)──RX(w2.1)─╰X─╭●────│───RX(w2.n)─╰X─╭●────│──┤  <Z>
-        2: ...........................................................
-        3: ──RX(xm)──RX(wm.1)───────╰X─╰●──RX(wm.n)───────╰X─╰●─┤  <Z>
+        0: ──RX(x1)──RX(w1.1)─╭●───────╭X──RX(w1.n)─╭●───────╭X─┤  \<Z\>  
+        1: ──RX(x2)──RX(w2.1)─╰X─╭●────│───RX(w2.n)─╰X─╭●────│──┤  \<Z\>  
+        2: ...........................................................  
+        3: ──RX(xm)──RX(wm.1)───────╰X─╰●──RX(wm.n)───────╰X─╰●─┤  \<Z\>  
     
     '''
     
@@ -52,7 +52,6 @@ class BasicEntangledCircuit:
             dev = qml.device("default.qubit", wires=n_qubits)
             warnings.warn(f"Dev has been set to None, setting it to {dev}")
 
-        
         self.n_qubits     = n_qubits
         self.n_layers     = n_layers
         self.aiframework  = aiframework
@@ -113,10 +112,10 @@ class StronglyEntangledCircuit:
         This class implements a torch/keras quantum layer using a strongly entangler
         circuit, as below:
 
-        0: ──RX(x1)──Rot(w1.1,w1.2,w1.3)─╭●───────╭X──Rot(w1.n-2,w1.n-1,w1.n)─╭●────╭X────┤  <Z>
-        1: ──RX(x2)──Rot(w2.1,w2.2,w2.3)─╰X─╭●────│───Rot(w2.n-2,w2.n-1,w2.n)─│──╭●─│──╭X─┤  <Z>
-        2: ................................................................................
-        3: ──RX(xm)──Rot(wm.1,wm.2,wm.3)───────╰X─╰●──Rot(wm.n-2,wm.n-1,wm.n)────╰X────╰●─┤  <Z>
+        0: ──RX(x1)──Rot(w1.1,w1.2,w1.3)─╭●───────╭X──Rot(w1.n-2,w1.n-1,w1.n)─╭●────╭X────┤  <Z>  
+        1: ──RX(x2)──Rot(w2.1,w2.2,w2.3)─╰X─╭●────│───Rot(w2.n-2,w2.n-1,w2.n)─│──╭●─│──╭X─┤  <Z>  
+        2: ................................................................................  
+        3: ──RX(xm)──Rot(wm.1,wm.2,wm.3)───────╰X─╰●──Rot(wm.n-2,wm.n-1,wm.n)────╰X────╰●─┤  <Z>  
     '''
 
     def __init__(self, n_qubits : int, n_layers : int, aiframework : str, dev : qml.device = None) -> None:
@@ -151,7 +150,6 @@ class StronglyEntangledCircuit:
             dev = qml.device("default.qubit", wires=n_qubits)
             warnings.warn(f"Dev has been set to None, setting it to {dev}")
 
-        
         self.n_qubits     = n_qubits
         self.n_layers     = n_layers
         self.aiframework  = aiframework
