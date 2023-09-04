d4cd4cc678ae4c9c9d54dd32dcc696f85ac6f210
alessandrosebastianelli <alessandroseba1995@gmail.com>
2023-09-04 14:09:42 +0200

    new structure + comments

--- hqm/utils/printer.py
+++ hqm/utils/printer.py
@@ -1,8 +1,34 @@
+import sys
+sys.path += ['.', './circuits/']
+
 import pennylane as qml
 import numpy as np
 
-def print_circuit(circuit):
-    weights = np.random.random(size=circuit.weight_shape['weights'])
-    inputs  = np.random.random(size=(circuit.n_qubits))
-    print(qml.draw(circuit.circuit, expansion_strategy="device")(inputs, weights))
+from hqm.circuits.circuit import QuantumCircuit
 
+class Printer:
+    '''
+        This class contains static methods to print quantum circuit information.
+    '''
+
+    @staticmethod
+    def draw_circuit(circuit : QuantumCircuit) -> str:
+        '''
+            Draw circuit structure.  
+
+            Parameters:  
+            -----------  
+            - circuit : hqm.circuits.circuit.QuantumCircuit  
+                hqm circuit to be drawn  
+            
+            Return:  
+            -------  
+            - str_circ : str  
+                string containing circuit structure  
+
+        '''
+        weights  = np.random.random(size=circuit.weight_shape['weights'])
+        inputs   = np.random.random(size=(circuit.n_qubits))
+        str_circ = qml.draw(circuit.circuit, expansion_strategy="device")(inputs, weights)
+        print(str_circ)
+        return str_circ
