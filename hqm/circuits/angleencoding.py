db6eb0186873e15baffaf45adffd5fd9be393e33
alessandrosebastianelli <alessandroseba1995@gmail.com>
2023-09-04 15:59:35 +0200

    adding random circuit

--- hqm/circuits/angleencoding.py
+++ hqm/circuits/angleencoding.py
@@ -41,9 +41,9 @@ class BasicEntangledCircuit(QuantumCircuit):
         self.weight_shape = {"weights": (n_layers, n_qubits)}
         self.circuit      = self.circ(self.dev, self.n_qubits)
         self.qlayer       = AIInterface.network_layer(circuit = self.circuit, 
-                                         weight_shape = self.weight_shape, 
-                                         n_qubits     = self.n_qubits, 
-                                         aiframework  = self.aiframework)
+                                        weight_shape = self.weight_shape, 
+                                        n_qubits     = self.n_qubits, 
+                                        aiframework  = self.aiframework)
 
     @staticmethod
     def circ(dev : qml.devices, n_qubits : int) -> FunctionType:
@@ -120,10 +120,11 @@ class StronglyEntangledCircuit(QuantumCircuit):
         
         self.weight_shape = {"weights": (n_layers, n_qubits, 3)}
         self.circuit      = self.circ(self.dev, self.n_qubits)
-        self.qlayer       = AIInterface.network_layer(circuit = self.circuit, 
-                                         weight_shape = self.weight_shape, 
-                                         n_qubits     = self.n_qubits, 
-                                         aiframework  = self.aiframework)
+        self.qlayer       = AIInterface.network_layer(
+                                        circuit = self.circuit, 
+                                        weight_shape = self.weight_shape, 
+                                        n_qubits     = self.n_qubits, 
+                                        aiframework  = self.aiframework)
         
     @staticmethod
     def circ(dev : qml.devices, n_qubits : int) -> FunctionType:
@@ -164,6 +165,87 @@ class StronglyEntangledCircuit(QuantumCircuit):
             '''
             qml.AngleEmbedding(inputs, wires=range(n_qubits))
             qml.StronglyEntanglingLayers(weights, wires=range(n_qubits))
+            measurements = [qml.expval(qml.PauliZ(wires=i)) for i in range(n_qubits)]
+            return measurements
+    
+        return qnode
+
+class RandomCircuit(QuantumCircuit):
+    '''
+        This class implements a torch/keras quantum layer using a random
+        circuit. 
+    '''
+    
+    def __init__(self, n_qubits : int, n_layers : int, aiframework : str, dev : qml.devices = None) -> None:
+        '''
+            RandomCircuit constructor.  
+
+            Parameters:  
+            -----------
+            - n_qubits : int  
+                number of qubits for the quantum circuit  
+            - n_layers : int  
+                number of layers for the quantum circuit  
+            - aiframework : str  
+                string representing the AI framework in use, can be 'torch' or 'keras'. This will create  
+                a compatible trainable layer for the framework.   
+            - dev : qml.device  
+                PennyLane device on wich run quantum operations (dafault : None). When None it will be set
+                to 'default.qubit'  
+            
+            Returns:  
+            --------  
+            Nothing, a RandomCircuit object will be created.  
+        '''
+        super().__init__(n_qubits=n_qubits, n_layers=n_layers, aiframework=aiframework, dev=dev)
+               
+        self.weight_shape = {"weights": (n_layers, n_qubits)}
+        self.circuit      = self.circ(self.dev, self.n_qubits)
+        self.qlayer       = AIInterface.network_layer(
+                                        circuit = self.circuit, 
+                                        weight_shape = self.weight_shape, 
+                                        n_qubits     = self.n_qubits, 
+                                        aiframework  = self.aiframework)
+
+    @staticmethod
+    def circ(dev : qml.devices, n_qubits : int) -> FunctionType:
+        '''
+            RandomCircuit static method that implements the quantum circuit.  
+
+            Parameters:  
+            -----------  
+            - dev : qml.device  
+                PennyLane device on wich run quantum operations (dafault : None). When None it will be set  
+                to 'default.qubit'  
+            - n_qubits : int  
+                number of qubits for the quantum circuit  
+
+            Returns:  
+            --------  
+            - qnode : qml.qnode  
+                the actual PennyLane circuit   
+        '''
+        @qml.qnode(dev)
+        def qnode(inputs : np.ndarray, weights : np.ndarray) -> list:
+            '''
+                PennyLane based quantum circuit composed of an angle embedding layer and a basic entangler
+                layer.  
+
+                Parameters:  
+                -----------  
+                - inputs : np.ndarray  
+                    array containing input values (can came from previous torch/keras layers or quantum layers)  
+                - weights : np.ndarray  
+                    array containing the weights of the circuit that are tuned during training, the shape of this
+                    array depends on circuit's layers and qubits.   
+                
+                Returns:  
+                --------  
+                - measurements : list  
+                    list of values measured from the quantum circuits  
+            '''
+            qml.AngleEmbedding(inputs, wires=range(n_qubits))
+            qml.RandomLayers(weights, wires=range(n_qubits))
             measurements = [qml.expval(qml.PauliZ(wires=i)) for i in range(n_qubits)]
             return measurements
     
