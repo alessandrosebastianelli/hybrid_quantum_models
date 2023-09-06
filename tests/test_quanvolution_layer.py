dc0c27edd88b09a11c5c438488b115a814be1b84
alessandrosebastianelli <alessandroseba1995@gmail.com>
2023-09-06 09:17:36 +0200

    adding lightning.qubit as dev

--- tests/test_quanvolution_layer.py
+++ tests/test_quanvolution_layer.py
@@ -5,12 +5,10 @@ from hqm.circuits.angleencoding import BasicEntangledCircuit
 from hqm.layers.quanvolution import Quanvolution2D
 from hqm.utils.printer import Printer
 
-
 import pennylane as qml
 import numpy as np
 import argparse
 import torch
-
 
 # Set random seeds
 torch.manual_seed(42)
@@ -26,21 +24,17 @@ if __name__ == '__main__':
     # Reading arguments
     args = parser.parse_args()
     
-    
     N_QUBITS      = int(args.n_qubits)
     N_LAYERS      = int(args.n_layers)
 
-
-
     if N_QUBITS < 1:       raise Exception('n_qubits must be greater than 1, found {N_QUBITS}')
     if N_LAYERS < 1:       raise Exception('n_layers must be greater than 1, found {N_LAYERS}')
-
 
     #=======================================================================
     # Inizialize Hybrid Model
     #=======================================================================
     print('Initializing hybrid model', '\n')
-    dev = qml.device("default.qubit", wires=N_QUBITS)
+    dev = qml.device("lightning.qubit", wires=N_QUBITS)
 
     qcircuit = BasicEntangledCircuit(n_qubits=N_QUBITS, n_layers=N_LAYERS, dev=dev)
     quanv    = Quanvolution2D(qcircuit=qcircuit, filters=9, kernelsize=3, stride=1)
@@ -49,7 +43,7 @@ if __name__ == '__main__':
     #=======================================================================
     # Applying Quanvolution2D
     #=======================================================================
-    print('Applying Quanvolution2D', '\n')
+    print('\nApplying Quanvolution2D', '\n')
     x = torch.rand(size=(2,3,16,16))
-    o =  quanv.forward(x)
+    o =  quanv(x)
     print('Input Shape', x.shape, 'Output shape', o.shape)
