25c5c067a5c0c876da134b6a8cdd926b720e2f47
alessandrosebastianelli <alessandroseba1995@gmail.com>
2023-09-11 10:52:12 +0200

    tests for amplitude encoding circuits

---
+++ tests/test_amplitudeencoding_circuits.py
@@ -0,0 +1,35 @@
+import sys
+sys.path += ['.', './hqm/']
+
+from hqm.circuits.amplitudeencoding import BasicEntangledCircuit, StronglyEntangledCircuit, RandomCircuit
+
+import numpy as np
+
+def test_BasicEntangledCircuit_output():
+    n_qubits = 2
+    n_layers = 1
+    circ = BasicEntangledCircuit(n_qubits=n_qubits, n_layers=n_layers).circuit
+    cinput   = np.random.randn((2**n_qubits))
+    cweights = np.zeros((n_layers,n_qubits))
+    out      = circ(cinput, cweights)
+    assert len(out) == n_qubits
+
+
+def test_StronglyEntangledCircuit_output():
+    n_qubits = 2
+    n_layers = 1
+    circ = StronglyEntangledCircuit(n_qubits=n_qubits, n_layers=n_layers).circuit
+    cinput   = np.random.randn((2**n_qubits))
+    cweights = np.zeros((n_layers,n_qubits, 3))
+    out      = circ(cinput, cweights)
+    assert len(out) == n_qubits
+
+
+def test_RandomCircuit_output():
+    n_qubits = 2
+    n_layers = 1
+    circ = RandomCircuit(n_qubits=n_qubits, n_layers=n_layers).circuit
+    cinput   = np.random.randn((2**n_qubits))
+    cweights = np.zeros((n_layers,n_qubits))
+    out      = circ(cinput, cweights)
+    assert len(out) == n_qubits
