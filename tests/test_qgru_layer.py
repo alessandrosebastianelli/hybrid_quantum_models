9ffba7b2f6bebf7ed57068a507314e474a1977b9
alessandrosebastianelli <alessandroseba1995@gmail.com>
2023-09-11 11:25:50 +0200

    test for recurrent layer

--- tests/test_qgru_layer.py
+++ tests/test_qgru_layer.py
@@ -10,31 +10,19 @@ import numpy as np
 import argparse
 import torch
 
-# Set random seeds
-torch.manual_seed(42)
-np.random.seed(42)
 
-if __name__ == '__main__':
-
-    parser = argparse.ArgumentParser(description='Test QGRU n layer')
-    # Model's arguments
-    parser.add_argument('--n_qubits',               type=int,   default=9)
-    parser.add_argument('--n_layers',               type=int,   default=1)
-
-    # Reading arguments
-    args = parser.parse_args()
-    
-    N_QUBITS      = int(args.n_qubits)
-    N_LAYERS      = int(args.n_layers)
-
-    if N_QUBITS < 1:       raise Exception('n_qubits must be greater than 1, found {N_QUBITS}')
-    if N_LAYERS < 1:       raise Exception('n_layers must be greater than 1, found {N_LAYERS}')
+def test_qgru_output():
+    N_QUBITS      = 9
+    N_LAYERS      = 1
+    INPUTSIZE     = 1
+    HIDDENSIZE    = 10
+    TIME_DIM      = 20
+    BATCH_SIZE    = 16
 
     #=======================================================================
     # Inizialize Hybrid Model
     #=======================================================================
-    print('Initializing hybrid model', '\n')
-    dev = qml.device("lightning.qubit", wires=N_QUBITS)
+    dev = qml.device("lightning.qubit", wires=N_QUBITS, shots = 1000)
 
     qcircuit1 = BasicEntangledCircuit(n_qubits=N_QUBITS, n_layers=N_LAYERS, dev=dev)
     qcircuit2 = BasicEntangledCircuit(n_qubits=N_QUBITS, n_layers=N_LAYERS, dev=dev)
@@ -42,13 +30,9 @@ if __name__ == '__main__':
 
     qcircuits = [qcircuit1, qcircuit2, qcircuit3]
     
-    qgru      = QGRU(qcircuits=qcircuits, inputsize=1, hiddensize=10)
-    Printer.draw_circuit(qcircuit1)
+    qgru      = QGRU(qcircuits=qcircuits, inputsize=INPUTSIZE, hiddensize=HIDDENSIZE)
 
-    #=======================================================================
-    # Applying Quanvolution2D
-    #=======================================================================
-    print('\nApplying Quanvolution2D', '\n')
-    x = torch.rand(size=(1,20,1))
+    x = torch.rand(size=(BATCH_SIZE,TIME_DIM,INPUTSIZE))
     o =  qgru(x)
-    print('Input Shape', x.shape, 'Output shape', o.shape)
+
+    assert o.shape == (BATCH_SIZE,TIME_DIM,HIDDENSIZE)
