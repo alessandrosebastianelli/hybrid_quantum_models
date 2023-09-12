e0ff209fee929eb9b7024354d4ab4a17fb35a812
alessandrosebastianelli <alessandroseba1995@gmail.com>
2023-09-12 13:07:20 +0200

    adding test for bellman circuit

---
+++ tests/test_custom_circuits.py
@@ -0,0 +1,25 @@
+import sys
+sys.path += ['.', './hqm/']
+
+from hqm.circuits.customcircuits import BellmanCircuit
+
+import numpy as np
+
+def test_BasicEntangledCircuit_angle_output():
+    n_qubits = 2
+    n_layers = 1
+    circ     = BellmanCircuit(n_qubits=n_qubits, n_layers=n_layers, encoding='angle').circuit
+    cinput   = np.random.randn(n_qubits)
+    cweights = np.random.randn(n_layers, n_qubits, 3)
+    out      = circ(cinput, cweights)
+    assert len(out) == n_qubits
+
+
+def test_BasicEntangledCircuit_amplitude_output():
+    n_qubits = 2
+    n_layers = 1
+    circ     = BellmanCircuit(n_qubits=n_qubits, n_layers=n_layers, encoding='amplitude').circuit
+    cinput   = np.random.randn(2**n_qubits)
+    cweights = np.random.randn(n_layers, n_qubits, 3)
+    out      = circ(cinput, cweights)
+    assert len(out) == n_qubits
