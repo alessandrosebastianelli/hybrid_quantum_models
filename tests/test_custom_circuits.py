34056e780917f08122de1ea396a71e1663a44f0f
alessandrosebastianelli <alessandroseba1995@gmail.com>
2023-09-12 13:22:03 +0200

    test for real amplitudes circuit;

--- tests/test_custom_circuits.py
+++ tests/test_custom_circuits.py
@@ -1,7 +1,7 @@
 import sys
 sys.path += ['.', './hqm/']
 
-from hqm.circuits.customcircuits import BellmanCircuit
+from hqm.circuits.customcircuits import BellmanCircuit, RealAmplitudesCircuit
 
 import numpy as np
 
@@ -19,6 +19,25 @@ def test_BasicEntangledCircuit_amplitude_output():
     n_qubits = 2
     n_layers = 1
     circ     = BellmanCircuit(n_qubits=n_qubits, n_layers=n_layers, encoding='amplitude').circuit
+    cinput   = np.random.randn(2**n_qubits)
+    cweights = np.random.randn(n_layers, n_qubits, 3)
+    out      = circ(cinput, cweights)
+    assert len(out) == n_qubits
+
+def test_RealAmplitudesCircuitt_angle_output():
+    n_qubits = 2
+    n_layers = 1
+    circ     = RealAmplitudesCircuit(n_qubits=n_qubits, n_layers=n_layers, encoding='angle').circuit
+    cinput   = np.random.randn(n_qubits)
+    cweights = np.random.randn(n_layers, n_qubits, 3)
+    out      = circ(cinput, cweights)
+    assert len(out) == n_qubits
+
+
+def test_RealAmplitudesCircuit_amplitude_output():
+    n_qubits = 2
+    n_layers = 1
+    circ     = RealAmplitudesCircuit(n_qubits=n_qubits, n_layers=n_layers, encoding='amplitude').circuit
     cinput   = np.random.randn(2**n_qubits)
     cweights = np.random.randn(n_layers, n_qubits, 3)
     out      = circ(cinput, cweights)
