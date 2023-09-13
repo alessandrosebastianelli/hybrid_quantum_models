ba554576cacf7053403e60b08040a4d872f59f43
alessandrosebastianelli <alessandroseba1995@gmail.com>
2023-09-13 16:27:12 +0200

    adding new utils func

---
+++ hqm/utils/sizes.py
@@ -0,0 +1,23 @@
+def size_conv_layer(self, s : int, kernel_size : int, padding : int, stride : int) -> int:
+        '''
+            Calculate the size of the image after convolution layer
+
+            Parameters:  
+            -----------  
+            - s : int  
+                integer represeting the size of one axis of the image  
+            - kernel_size : int  
+                integer represeting the size of the convolutional kernel  
+            - padding : int  
+                integer represeting the padding size  
+            - stride : int  
+                integer representing the stride size  
+  
+            Returns:  
+            --------  
+            - size : int  
+                size after conv2D and Maxpool  
+        '''
+
+        size = int(((s - kernel_size + 2 * padding)/stride) + 1)
+        return size
