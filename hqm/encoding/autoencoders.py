add4b0eff929ebb4ddba7a2d33820c7a5d1ebbe2
alessandrosebastianelli <alessandroseba1995@gmail.com>
2023-09-14 13:24:15 +0200

    hybrid autoencoer

--- hqm/encoding/autoencoders.py
+++ hqm/encoding/autoencoders.py
@@ -131,10 +131,138 @@ class QuanvolutionAutoencoder(torch.nn.Module):
         
 class HybridAutoencoder(torch.nn.Module): 
     '''
-        The encoder and the decoder are classical, the hidden space is processed by 
-        a quantum circuit.
+        Hybrdid convolutional autoencoder, the encoder and the decodere are composed of clasical layer, the hidenn space
+        is processed by a quantum circuit.
     '''
 
-    def __init__(self):
+    def __init__(self, qlayer : BasicLayer, in_shape : tuple[int], filters : list[int], kernelsizes : list[int], strides : list[int]) -> None:
+        '''
+            QuanvolutionAutoencoder constructor.  
+
+            Parameters:  
+            -----------
+            - qlayer : hqm.layers.basilayer.BasicLayer  
+                hqm quantum layer to be stacked between two fully connected layers
+            - in_shape : tuple 
+                tuple represeting the image shape, (width, height, channels)
+            - filters : list
+                list containing the number of filters for each quanvolution layer
+            - kernelsizes: list
+                list containing the kernelsize for each quanvolution layer
+            - strides: list
+                list containin the stride for each quanvolution layer  
+            
+            Returns:  
+            --------  
+            Nothing, a QuanvolutionAutoencoder object will be created. 
+        '''
+
         super().__init__()
+
+        if len(filters)     != len(kernelsizes): raise Exception(f"lenght of filters must be the same of lenght of kernelsizes, found {len(filters)} and {len(kernelsizes)}")
+        if len(filters)     != len(strides):     raise Exception(f"lenght of filters must be the same of lenght of strides, found {len(filters)} and {len(strides)}")
+        if len(strides)     != len(kernelsizes): raise Exception(f"lenght of strides must be the same of lenght of kernelsizes, found {len(strides)} and {len(kernelsizes)}")
+        if len(in_shape)    != 3:                raise Exception(f"length of in_shape must be equals to 3 (widht, height, channels, found {len(in_shape)}")
+        
+        self.encoder  = []
+        self.decoder  = []
+        self.sizes    = []
+        
+        w, h, c       = in_shape
+        self.depth    = len(filters)
+
+        self.encoder.append(torch.nn.Conv2d(c, filters[0], kernel_size=kernelsizes[0], stride=strides[0]))
+        w = size_conv_layer(s=w, kernel_size=kernelsizes[0], padding=0, stride=strides[0])
+        h = size_conv_layer(s=h, kernel_size=kernelsizes[0], padding=0, stride=strides[0])
+        c = filters[0]
+        # Building the quanvolution encoder
+        for i in range(1, self.depth):
+            self.encoder.append(torch.nn.Conv2d(filters[i-1], filters[i], kernel_size=kernelsizes[i], stride=strides[i]))
+            w = size_conv_layer(s=w, kernel_size=kernelsizes[i], padding=0, stride=strides[i])
+            h = size_conv_layer(s=h, kernel_size=kernelsizes[i], padding=0, stride=strides[i])
+            c = filters[i]
+        
+        self.w            = w
+        self.h            = h
+        self.c            = c
+        self.flatten_size = w*h*c
+        self.fc1          = torch.nn.Linear(self.flatten_size, qlayer.n_qubits)
+        self.qc_1         = qlayer.qlayer
+        self.fc2          = torch.nn.Linear(qlayer.n_qubits, self.flatten_size)
+        
+        filters.reverse()
+        filters.append(filters[-1])
+        kernelsizes.reverse()
+        strides.reverse()
+
+        # Building the classical decoder
+        for i in range(self.depth):
+            self.decoder.append(torch.nn.ConvTranspose2d(filters[i], filters[i+1], kernel_size=kernelsizes[i], stride=strides[i]))
+        
+
+    def encoder_f(self, x : torch.Tensor) -> torch.Tensor:
+        '''
+            Torch forward method for the encoder
+
+            Parameters:  
+            -----------
+            - x : torch.Tensor  
+                input for the torch model  
+
+            Returns:  
+            --------  
+            - out : torch.Tensor  
+                output from the torch model  
+        '''
+
+        for i in range(self.depth):
+            x = torch.nn.functional.relu(self.encoder[i](x))
+
+        x = x.view(-1, self.flatten_size)
+        x = torch.nn.functional.relu(self.fc1(x))
+        x = torch.nn.functional.relu(self.qc_1(x))
+        x = torch.nn.functional.relu(self.fc2(x))
+        out = x.reshape((x.shape[0], self.c, self.w, self.h))
+        return out
+
+    def decoder_f(self, x : torch.Tensor) -> torch.Tensor:
+        '''
+            Torch forward method for the decoder
+
+            Parameters:  
+            -----------
+            - x : torch.Tensor  
+                input for the torch model  
+
+            Returns:  
+            --------  
+            - out : torch.Tensor  
+                output from the torch model  
+        '''
+
+        for i in range(self.depth - 1):
+            x = torch.nn.functional.relu(self.decoder[i](x))
+        out = torch.nn.functional.sigmoid(self.decoder[-1](x))
+        
+        return out
+
+    def forward(self, x : torch.Tensor) -> torch.Tensor:
+        '''
+            Torch forward method  
+
+            Parameters:  
+            -----------
+            - x : torch.Tensor  
+                input for the torch model  
+
+            Returns:  
+            --------  
+            - out : torch.Tensor  
+                output from the torch model  
+        '''
+
+        dec = self.encoder_f(x)
+        out = self.decoder_f(dec)
+        
+        return out
         
